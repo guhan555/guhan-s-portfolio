54d0655a559eefc0f48e0987582224f876c0910d
Guhan S <guhan079@gmail.com>
2026-03-15 18:18:34 +0530

    Update basePath for assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
+  basePath: "/guhan-s-portfolio",
+  assetPrefix: "/guhan-s-portfolio/",
   images: {
     unoptimized: true,
   },
